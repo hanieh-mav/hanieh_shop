c88ed37ee176cf33a03fa6a99d9b9fcdc1f52d69
hanieh <h.mehdiabadi@gmail.com>
2021-06-10 17:11:00 +0430

    add comment system

--- config/urls.py
+++ config/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('shop.urls')),
     path('accounts/',include('accounts.urls')),
+    path('comment/',include('comment.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
